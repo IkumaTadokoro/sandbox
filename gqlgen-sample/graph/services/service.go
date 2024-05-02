36e145c7d2ba3524e19d792b52b25c3ec0f5aac1
ikuma-t <tadokorodev@gmail.com>
2024-05-02 16:34:13 +0900

    feat: repositories Query のリゾルバを実装

--- gqlgen-sample/graph/services/service.go
+++ gqlgen-sample/graph/services/service.go
@@ -11,16 +11,23 @@ type UserService interface {
 	GetUserByName(ctx context.Context, name string) (*model.User, error)
 }
 
+type RepoService interface {
+	GetRepoByFullName(ctx context.Context, owner, name string) (*model.Repository, error)
+}
+
 type Services interface {
 	UserService
+	RepoService
 }
 
 type services struct {
 	*userService
+	*repoService
 }
 
 func New(exec boil.ContextExecutor) Services {
 	return &services{
 		userService: &userService{exec: exec},
+		repoService: &repoService{exec: exec},
 	}
 }
