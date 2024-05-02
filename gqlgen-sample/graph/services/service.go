4b64109da73302a39f2c72bae2fb7fcd843ac168
ikuma-t <tadokorodev@gmail.com>
2024-05-02 16:56:10 +0900

    feat: 分割した Issues リゾルバを定義

--- gqlgen-sample/graph/services/service.go
+++ gqlgen-sample/graph/services/service.go
@@ -15,19 +15,26 @@ type RepoService interface {
 	GetRepoByFullName(ctx context.Context, owner, name string) (*model.Repository, error)
 }
 
+type IssueService interface {
+	GetIssueByNumber(ctx context.Context, repoID string, number int) (*model.Issue, error)
+}
+
 type Services interface {
 	UserService
 	RepoService
+	IssueService
 }
 
 type services struct {
 	*userService
 	*repoService
+	*issueService
 }
 
 func New(exec boil.ContextExecutor) Services {
 	return &services{
-		userService: &userService{exec: exec},
-		repoService: &repoService{exec: exec},
+		userService:  &userService{exec: exec},
+		repoService:  &repoService{exec: exec},
+		issueService: &issueService{exec: exec},
 	}
 }
