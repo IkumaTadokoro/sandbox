d1e4f3bcc22cf5277c2df5c15bf1b43da9ad6324
ikuma-t <tadokorodev@gmail.com>
2024-05-04 21:53:32 +0900

    feat: add loading skeleton

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import { lusitana } from '../ui/fonts';
+import { lusitana } from '../../ui/fonts';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
